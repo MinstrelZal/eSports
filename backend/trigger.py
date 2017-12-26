dbc8a9521889df6e4ed69e6dd0be31f7b1b585e8
狐狐 <15600705511@163.com>
2017-12-26 15:32:20 +0800

    Add files via upload
    修复了一些bug

--- backend/trigger.py
+++ backend/trigger.py
@@ -1,72 +1,68 @@
 import pymysql
 
-db = pymysql.connect('localhost', 'root', '20121513zal', 'esports')
-cursor = db.cursor()
+db = pymysql.connect('localhost', 'root', 'huhuhu917531', 'test_db')
+cur = db.cursor()
 #db.set_charset('utf8')
 
 #创建trigger
 #  1.删除esport_info条目后，删除game_info和league_info中与删除的esport_info有关的信息
 #  2.删除league_info条目后，删除league_info包含的所有game_info信息，和league_result中有关信息
 #  3.删除team_info 条目后， 删除player_info属于team_info的选手信息
-def trigger_init(db,cursor) :
-    sql1 = "create trigger \
-        if not exists delete_all_esport \
-        after delete on esport_info \
-        referencing \
-           newrow as Newesport \
-        for each row \
-        begin \
-           delete from game_info \
-              where game_info.league_id = ALL \
-                 ( \
-                     select id \
-                     from league_info \
-                     where league_info.id = ALL \
-                     ( \
-                        select league_id \
-                        from league_result \
-                        where league_result.esport_id = Newesport.id \
-                        ) \
-                  ); \
-           delete from league_info \
-              where league_info.id = ALL \
-              (\
-                  select league_id \
-                  from league_result \
-                  where league_result.esport_id = Newesport.id \
-            ); \
+cur.execute("drop trigger if exists delete_all_esport")
+sql = """
+           create trigger delete_all_esport
+           before delete on esport_info 
+           for each row 
+           begin 
+           delete from game_info 
+              where game_info.league_id = ALL 
+                 ( 
+                     select id 
+                     from league_info 
+                     where league_info.id = ALL 
+                     ( 
+                        select league_id 
+                        from league_result 
+                        where league_result.esport_id = old.id 
+                        ) 
+                  ); 
+           delete from league_info 
+              where league_info.id = ALL 
+              (
+                  select league_id 
+                  from league_result 
+                  where league_result.esport_id = old.id 
+            ); 
+           delete from league_result 
+           where league_result.esport_id = old.id ;
+           end"""
+
+cur.execute(sql)
+
+    #cursor.execute("drop trigger delete_all_game on league_info if exists delete_all_game")
+cur.execute("drop trigger if exists delete_all_game")
+sql = "    create trigger delete_all_game\
+           before delete on league_info \
+           for each row \
+           begin \
            delete from league_result \
-           where league_result.esport_id = Newesport.id ;\
-         end;"
-
-    cursor.execute(sql1)
-
-    sql1 = "create trigger \
-        if not exists delete_all_game \
-        after delete on league_info \
-        referencing \
-           newrow as Newleague \
-        for each row \
-        begin\
-           delete from league_result \
-           where league_result.league_id = Newleague.id ;\
+           where league_result.league_id = old.id ;\
            delete from game_info \
-           where game_info.league_id = Newleague.id; \
-        end;"        
+           where game_info.league_id = old.id;\
+           end"        
 
-    cursor.execute(sql1)
+cur.execute(sql)
 
-    sql1 = "create trigger \
-        if not exists delete_player \
-        after delete on team_info \
-        referencing \
-           newrow as Newplayer \
+    #cursor.execute("drop trigger delete_player on team_info if exists delete_player")
+cur.execute("drop trigger if exists delete_player")
+sql = "create trigger delete_player\
+        before delete on team_info \
         for each row \
-        begin \
            delete from player_info \
-           where player_info.team_id = Newteam.id \
-        end;"
+           where player_info.team_id = old.id"
 
-    cursor.execute(sql1)
+cur.execute(sql)
 
-trigger_init(db, cursor)
+
+
+
